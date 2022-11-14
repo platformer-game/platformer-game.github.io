e30dd64773e32c24e102e857cd398962b5fc81fe
platformer-game <112494719+platformer-game@users.noreply.github.com>
2022-11-14 12:56:44 +0100

    Delete localtime directory

--- localtime/localtime.js
+++
@@ -1,4 +0,0 @@
-var d = new Date();
-var localtime = (d.getMonth()+1)  + "/" + d.getDate() + "/" + d.getFullYear() + " " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
-
-document.write(localtime);
