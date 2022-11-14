49a5ca5e451aac283daa1bcafbc72db085cbf381
platformer-game <112494719+platformer-game@users.noreply.github.com>
2022-11-14 12:57:21 +0100

    add localtime folder
    add localtime folder

---
+++ localtime/localtime.js
@@ -0,0 +1,4 @@
+var d = new Date();
+var localtime = (d.getMonth()+1)  + "/" + d.getDate() + "/" + d.getFullYear() + " " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+
+document.write(localtime);
